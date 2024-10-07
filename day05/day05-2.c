8f3fd88ea76bd9e9892db6f88e2645055e048d48
hariboeim <won082005@naver.com>
2024-10-07 16:02:48 +0900

    Create day05-2.c

---
+++ day05/day05-2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student {
+	int num;
+	char name[100];
+	int score;
+};
+
+int main() {
+
+	int N;
+	struct Student person[2];
+
+	person[0].num = 1111;
+	strcpy_s(person[0].name, 100, "Ã¶¼ö");
+	person[0].score = 100;
+
+	printf("%d, %s, %d", person[0].num, person[0].name, person[0].score);
+
+
+	return 0;
+}
